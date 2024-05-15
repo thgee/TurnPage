9ef1d476df1d91515777d3e04d1488b90bf9af1a
thgee <thgeee@naver.com>
2024-05-16 02:33:17 +0900

    FEAT: Kakao Login 인가코드 발급 구현

--- src/components/LoginBtn/KakaoBtn/KakaoLoginBtn.tsx
+++ src/components/LoginBtn/KakaoBtn/KakaoLoginBtn.tsx
@@ -1,6 +1,6 @@
 const KakaoLoginBtn = ({ className }: { className?: string }) => {
   const handleLogin = () => {
-    window.location.href = `${process.env.REACT_APP_DOMAIN}/oauth2/authorization/kakao`;
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAO_API_KEY}&redirect_uri=${process.env.REACT_APP_DOMAIN}/loginredirect`;
   };
   return (
     <>
--- src/pages/Login/LoginRedirect.tsx
+++ src/pages/Login/LoginRedirect.tsx
@@ -2,16 +2,20 @@ import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 const LoginRedirect = () => {
-  // 쿼리스트링에서 인가코드 받아와서 백엔드로 넘기기
-  // 백엔드에서 토큰 방아와서 localstorage에 저장한 후 페이지 이동
+  // 1. 쿼리스트링에서 인가코드 받아와서 백엔드로 넘기기
+  // 2. 백엔드에서 토큰 받아와서 localstorage에 저장한 후 페이지 이동
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   // 인가코드
-  console.log(searchParams.get("code"));
+  const authCode = searchParams.get("code");
 
-  useEffect(() => {}, []);
-  return <>spinner</>;
+  useEffect(() => {
+    // API로 인가코드 보내고 토큰 받아오기
+    // 받아온 토큰을 localStorage에 저장
+    // 저장한 후 로그인 이전페이지로 이동 (뒤로가기 두번?)
+  }, []);
+  return <>로그인 중..</>;
 };
 
 export default LoginRedirect;
